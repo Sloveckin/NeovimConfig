27dd79397cc8accb11a267341830cd624a502a83
Slava Rybin <Sloveckin2711@gmail.com>
2025-05-08 18:37:04 +0300

    delete gitignore

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -13,6 +13,12 @@ require("neo-tree").setup({
               require("neo-tree.command").execute({ action = "close" })
             end
         },
-
-    }
+    },
+    filesystem = {
+        filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+        },
+    },
 })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,9 @@
 require'nvim-treesitter.configs'.setup{
   ensure_installed = {"c", "cpp", "lua"},
-  
   sync_install = false,
   auto_install = true,
   highlight = {
-    enable = true 
+    enable = true
   },
 
 }
